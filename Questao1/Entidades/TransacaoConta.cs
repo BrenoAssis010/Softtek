021062db2ced7b7af83faeeb4b110f4fb12509b9
BrenoDev8 <https://github.com/DeboraEGomes/Projeto-Integrado-Puc-Minas.git>
2023-01-11 07:02:12 -0300

    Resolução da questão um.

---
+++ Questao1/Entidades/TransacaoConta.cs
@@ -0,0 +1,36 @@
+﻿using Questao1.Enums;
+
+namespace Questao1.Entidades
+{
+    public class TransacaoConta
+    {
+        public TransacaoConta()
+        {
+
+        }
+
+        public TransacaoConta(double valorTransacao)
+        {
+            ValorTransacao = valorTransacao;
+        }
+
+        public double ValorTransacao { get; private set; }
+
+        public double TransacaoBancaria(double ValorTransacao, TipoTransacaoContaEnum TipoTransacaoConta)
+        {
+            switch (TipoTransacaoConta)
+            {
+                case TipoTransacaoContaEnum.Saque:
+                    ValorTransacao = ValorTransacao == 0 ? 0 : ValorTransacao + 3.5;
+                    break;
+                case TipoTransacaoContaEnum.Deposito:
+                    ValorTransacao = ValorTransacao == 0 ? 0 : ValorTransacao;
+                    break;
+                default:
+                    break;
+            }
+
+            return ValorTransacao;
+        }
+    }
+}
