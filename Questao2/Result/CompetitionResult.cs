3336a5c21c832cba1457ef31c4f4cf398c8b3d9e
BrenoDev8 <https://github.com/DeboraEGomes/Projeto-Integrado-Puc-Minas.git>
2023-01-13 07:42:24 -0300

    Desenvolvido busca de gols por parametros.

---
+++ Questao2/Result/CompetitionResult.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace Questao2.Result
+{
+    public class CompetitionResult
+    {
+        [JsonProperty("page")]
+        public int Page { get; set; }
+
+        [JsonProperty("per_page")]
+        public int PerPage { get; set; }
+
+        [JsonProperty("total")]
+        public int Total { get; set; }
+
+        [JsonProperty("pages")]
+        public int TotalPages { get; set; }
+
+        [JsonProperty("data")]
+        public List<TeamResult> Data { get; set; }
+    }
+}
